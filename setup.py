ba357da35b30aa839b0da5ebdc0c2da0234c48be
sivaji <sivaji.koppisetti@oqlo.ai>
2024-08-06 12:27:47 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'sivaji koppisetti',
+    author_email= 'sivajikoppisetti@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
